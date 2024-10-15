900de1d2e0e291f2f2baa09b050bfd2dee6a71a8
Rasnux <rasnux@localhost.localdomain>
2024-10-16 00:07:46 +0530

    locomo

--- src/Partner.jsx
+++ src/Partner.jsx
@@ -6,13 +6,25 @@ function Partner() {
     <div id='partner'>
         {/* <h1>Partners</h1> */}
         <div className="partnerContent">
-            {/* <img src="/pepsicola.png" className='logos'/>
-            <img src="/monsterlogo.png" className='logos'/>
-            <img src="/cocalogo.png" className='logos'/>
-            <img src="/spritelogo.png" className='logos'/>
-            <img src="/redbulllogo.png" className='logos'/> */}
+            <img src="/pepsicola.png" className='logos'/>
+            <h2>Pepsi Cola</h2>
         </div>
-
+        <div className="partnerContent">
+            <img src="/monsterlogo.png" className='logos'/>
+            <h2>Monster Energy</h2>
+        </div>
+        <div className="partnerContent">
+            <img src="/cocalogo.png" className='logos'/>
+            <h2>Coca Cola</h2>
+        </div>
+        <div className="partnerContent">
+            <img src="/spritelogo.png" className='logos'/>
+            <h2>Sprite</h2>
+        </div>
+        <div className="partnerContent">
+            <img src="/redbulllogo.png" className='logos'/>
+            <h2>Red Bull</h2>
+        </div>
     </div>
   )
 }
